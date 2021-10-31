84b81fd8bad59a3ac88c7bb013a3880c6a8f717f
sanjit43 <sanjitsarker257@gmail.com>
2021-10-31 11:49:32 +0600

    contact section added

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -2,8 +2,23 @@ import React from 'react';
 
 const Contact = () => {
     return (
-        <div>
-            <h2>This is Contact!!</h2>
+        <div className='text-center border broder-2 border-primary m-5 p-5'>
+            <h2>CONTACT US</h2>
+            <div className='text-danger w-50' style={{ margin: '0 auto' }}><hr /></div>
+            <p className='text-center'>Our team of experts can help answer any questions you might have. Please fill out the form below <br /> and a consultant will respond shortly.</p>
+            <div className='text-center'>
+                <form>
+                    <input className='my-2' style={{ width: '300px' }} type="text" name="" id="" placeholder='Subject' />
+                    <br />
+                    <textarea className='my-2' style={{ width: '500px' }} name="" id="" cols="30" rows="10"></textarea>
+                    <br />
+                    <input className='mx-3 my-2' style={{ width: '300px' }} type="text" name="" id="" placeholder='Your Name' />
+                    <input style={{ width: '300px' }} type="email" name="" id="" placeholder='Email' />
+                </form>
+            </div>
+            <p className='mt-3'>Email:experts@asiatours.com
+                <br />
+                Hotline:(+84) 916 952 668</p>
         </div>
     );
 };
