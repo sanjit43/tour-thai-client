151b5af31197eed76f7f0840adb6d52c42f86b2d
sanjit43 <sanjitsarker257@gmail.com>
2021-10-31 12:46:04 +0600

    footer done

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className='bg-dark'>
+        <div className='bg-dark p-5'>
             <h3 className='text-white text-center'>AS SEEN IN...</h3>
             <p className='text-white text-center'>Copyright &copy; 2021. All information, logos, designs, text, photos, graphics... on this website are owned by TourThailand.com</p>
         </div>
