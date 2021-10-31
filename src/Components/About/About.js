369e842b105fd19696cc001c7c9b8d99eb92bb48
sanjit43 <sanjitsarker257@gmail.com>
2021-10-31 10:19:26 +0600

    about section added

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            <h2>This is About!!</h2>
+        <div className='border border-2 border-danger m-5 p-5'>
+            <div>
+                <h2 className='p-3'>Official Website of Thailand Tour</h2>
+                <p>We are proudly the Best Inside Travel DMCs & Tour Operators in each destination where you travel to, our local professional team is always by your side & take care of your trip from start to end to make your tour unforgettable experiences...</p>
+
+                <h3 className='p-3'>Our Broucher</h3>
+                <p className='p-3'>To help you prepare your trip and make the most of your time when in Asia, we would like to give you this pre-departure document. It will provide you a comprehensive guide to Asia before starting the Asia tour</p>
+            </div>
+
         </div>
     );
 };
